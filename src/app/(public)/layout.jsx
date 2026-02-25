daea5cccee45084cce1225e1a047113b86a592ff
NativeCode <tm.nicolasmachuca@gmail.com>
2026-02-25 16:28:14 -0300

    fix carrito

--- src/app/(public)/layout.jsx
+++ src/app/(public)/layout.jsx
@@ -2,15 +2,21 @@ import Navbar from "@/Componentes/Navbar";
 import FooterPremiumMedico from "@/Componentes/Footer";
 import ToasterClient from "@/Componentes/ToasterClient";
 import WhatsAppFloatButton from "@/Componentes/WhatsAppFloatButton";
+import CarritoProvider from "@/ContextosGlobales/CarritoContext";
+import ObjetoPagarProvider from "@/ContextosGlobales/ObjetoPagarContext";
 
 export default function PublicLayout({ children }) {
   return (
-    <div className="relative min-h-screen bg-black text-white">
-      <ToasterClient />
-      <Navbar />
-      <main className="relative pt-24 md:pt-28">{children}</main>
-      <FooterPremiumMedico />
-      <WhatsAppFloatButton />
-    </div>
+    <CarritoProvider>
+      <ObjetoPagarProvider>
+        <div className="relative min-h-screen bg-black text-white">
+          <ToasterClient />
+          <Navbar />
+          <main className="relative pt-24 md:pt-28">{children}</main>
+          <FooterPremiumMedico />
+          <WhatsAppFloatButton />
+        </div>
+      </ObjetoPagarProvider>
+    </CarritoProvider>
   );
 }
